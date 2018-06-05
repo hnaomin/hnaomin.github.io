2344c1889582bb761370d4711f76e075d1367dff
hnaomin <hyojin_noh@fitnyc.edu>
2018-06-05 16:30:52 -0400

    Delete menu.js

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    document.getElementById("sidenav").style.width = "205px";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
-} 
