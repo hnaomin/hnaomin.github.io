a2cb29fbbd61dd5c55e91e6a9a974a69d753e5d4
hnaomin <25823604+hnaomin@users.noreply.github.com>
2018-08-12 01:26:41 -0400

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -190,18 +190,17 @@ $(function(){$(".totopIcon").click(function(){$("html,body").animate({scrollTop:
 
 /* music */
 var playlistId = "PLFU2CdHB2e4dwetcYyTzCMZeHLk1SXR7a";
-var tag = document.createElement('script');
-		tag.src = "https://www.youtube.com/iframe_api";
+      var tag = document.createElement('script');
 
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    var player;
-
-    function onYouTubeIframeAPIReady() {
+      var player;
+      function onYouTubeIframeAPIReady() {
         player = new YT.Player('music', {
-            height: '200',
-            width: '200',
+          height: '200',
+          width: '200',
             playerVars: {
                 autoplay: 0,
                 controls: 1,
@@ -211,14 +210,15 @@ var tag = document.createElement('script');
                 loop: 1,
                 modestbranding: 0
             },
-            events: {
+          events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
-            }
+          }
         });
-    }
+      }
 
-    var playlistArray;
+/*shuffle*/
+var playlistArray;
     var playListArrayLength;
     var maxNumber;
 
@@ -239,7 +239,7 @@ function onPlayerReady(event) {
     player.cuePlaylist({
         'listType': 'playlist',
         'list': playlistId
-    });
+    })
 }
 
 var firstLoad = true;
